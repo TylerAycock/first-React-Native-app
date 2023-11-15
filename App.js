5213b1fdf4ff8ff00f3cd4b67236903614063fac
Tyler Aycock <tyler.joseph.aycock@gmail.com>
2023-11-15 11:06:16 -0800

    initial commit for new React Native App

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+  const [name, setName] = useState("Tyler")
+
+  const pressHandler = () => {
+    setName('Shawn')
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>My name is {name}</Text>
+      <Text></Text>
+      <View style={styles.buttoneContainer}>
+        <Button title='update state' onPress={pressHandler} />
+      </View>
     </View>
   );
 }
@@ -14,7 +23,23 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    color: 'red',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header: {
+    backgroundColor: 'pink',
+    padding: 20,
+  },
+  boldText: {
+    fontWeight: "bold"
+  },
+  body: {
+    backgroundColor: 'gold',
+    padding: 20,
+    fontWeight: "bold",
+  },
+  buttoneContainer: {
+    marginTop: 20,
+  }
 });
